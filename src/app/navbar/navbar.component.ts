ce46fe6e66d3bc7cd698b8660ad0265261e5b513
leoka2000 <reusoliveiraleo@gmail.com>
2025-10-10 12:15:44 +0200

    navbar improvement

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { NgIf, NgStyle } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,11 +8,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgIf],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgIf, NgStyle],
 })
 export class NavbarComponent {
   isMenuOpen = signal(false);
   showMobileToggle = signal(false); // default false
+isAtTop = signal(true); // true when at top of page
 
   constructor() {
     if (typeof window !== 'undefined') {
@@ -22,9 +23,14 @@ export class NavbarComponent {
       window.addEventListener('resize', () => {
         this.showMobileToggle.set(window.innerWidth <= 768);
       });
+
+      // Scroll listener
+      // Detect scroll position
+      window.addEventListener('scroll', () => {
+        this.isAtTop.set(window.scrollY === 0);
+      });
     }
   }
-
   toggleMenu() {
     this.isMenuOpen.set(!this.isMenuOpen());
   }
