7cae5302dee242082b9bfebe0e9bc08ffebf542b
leoka2000 <reusoliveiraleo@gmail.com>
2025-10-10 15:24:18 +0200

    icons

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButton, MatIconButton } from "@angular/material/button";
 import { CartButtonComponent } from '../cart-buttton/cart-buttton.component';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [MatButton, MatIconButton, CartButtonComponent],
+  imports: [MatButton, MatIconButton, CartButtonComponent, MatIcon],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
